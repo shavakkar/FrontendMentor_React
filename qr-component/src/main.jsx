9179e311488114709714515679d86ef5e194ed89
shavakkar <shavakkar@gmail.com>
2023-09-03 17:20:04 +0530

    Completed QR Component

---
+++ qr-component/src/main.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// // import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+// import React from "react";
+import { render } from "react-dom";
+// import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+render(<App />, root);
